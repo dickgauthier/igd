061d7446774760a96f0d87f11196e783dc8a7c59
dickgauthier <125268144+dickgauthier@users.noreply.github.com>
2024-04-17 08:45:01 -0400

    Update disk.cmd

---
+++ disk.cmd
@@ -0,0 +1,8 @@
+Cmd /C
+(Echo create vdisk file=c:\users\disk.vhd maximum=1000
+Echo select vdisk file=c:\users\disk.vhd
+Echo clean 
+Echo create partition primary 
+Echo format fs=fat32
+Echo assign letter=d
+) | cmd
